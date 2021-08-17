3fc42a50792b19aa48fa3969e6483164651e7dee
warren yun <xxwarrenhdxx@gmail.com>
2021-08-17 15:56:57 -0400

    Add meditation level data

--- src/main.py
+++ src/main.py
@@ -2,14 +2,15 @@ from telnetlib import Telnet
 import sys
 import json
 import time
-import random
 import csv
 
 tn = Telnet('localhost', 13854)
 tn.write('{"enableRawOutput": true, "format": "Json"}'.encode("utf-8"))
 
 avg_attention = 0
+avg_meditation = 0
 total_attention = 0
+total_meditation = 0
 iter = 0
 
 while True:
@@ -19,4 +20,11 @@ while True:
         iter += 1
         total_attention += dict['eSense']['attention']
         avg_attention = total_attention/iter
-        print(avg_attention)
+        total_meditation += dict['eSense']['meditation']
+        avg_meditation = total_meditation/iter
+        print("Average attention level: " + str(avg_attention))
+        print("Current attention level: " + str(dict['eSense']['attention']))
+        print("Average meditation level: " + str(avg_meditation))
+        print("Current meditation level: " + str(dict['eSense']['meditation']))
+        print("-------")
+
