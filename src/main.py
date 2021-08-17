3452a57ad5d69a363a1aeac664ea1baa5d2da0eb
warren yun <xxwarrenhdxx@gmail.com>
2021-08-17 15:11:38 -0400

    Get attention average

--- src/main.py
+++ src/main.py
@@ -1,13 +1,22 @@
 from telnetlib import Telnet
 import sys
 import json
+import time
+import random
+import csv
 
 tn = Telnet('localhost', 13854)
 tn.write('{"enableRawOutput": true, "format": "Json"}'.encode("utf-8"))
+
+avg_attention = 0
+total_attention = 0
+iter = 0
 
 while True:
     line=tn.read_until('\r'.encode("utf-8"))
     dict=json.loads(str(line.decode("utf-8")))
     if 'eSense' in dict:
-        print(dict['eSense'])
-    
+        iter += 1
+        total_attention += dict['eSense']['attention']
+        avg_attention = total_attention/iter
+        print(avg_attention)
